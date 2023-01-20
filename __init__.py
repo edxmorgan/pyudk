7f0edfee452d2163913a2a3067deaccb41161a5e
Edward Morgan <emorg31@lsu.edu>
2023-01-20 17:53:42 -0600

    flit init

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
-from .__version__ import __version__
+"""In classical mechanics, the Udwadia–Kalaba formulation is a method for deriving the
+equations of motion of a constrained mechanical system."""
+
+__version__ = '0.0.1'
 
 from .core import *
